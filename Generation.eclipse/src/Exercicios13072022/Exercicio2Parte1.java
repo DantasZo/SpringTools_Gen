a03f33cf1597250bb1138ed9d38fd69762209b2f
lucas <dantaslucas337@gmail.com>
2022-07-13 12:29:36 -0300

    Exercicio dois adicionado.

---
+++ Generation.eclipse/src/Exercicios13072022/Exercicio2Parte1.java
@@ -0,0 +1,32 @@
+package Exercicios13072022;
+
+public class Exercicio2Parte1 {
+	
+	public String Intmotor(String integridade) {
+	
+		
+		System.out.println("Como está o motor?");
+		
+		integridade = "O motor foi reparado e está 100%";
+	    System.out.println(integridade);	
+
+		
+		
+	return toString();
+	}
+	public String Piloto(String P) {
+		
+		System.out.println("Quem é o pilot?");
+		System.out.println("O piloto é o gabriel, voa a mais de 20 anos!");
+		
+		return toString();
+	}
+	public void combust(int niv) {
+	System.out.println("Qual o nivel do combustivel?");
+	System.out.println("O avião foi recentemente abastecido e está pronto!");
+    niv=100; 
+    System.out.println("Combustivel em: "+niv);
+	
+	
+	}
+}
---
+++ Generation.eclipse/src/Exercicios13072022/Exercicio2Parte2.java
@@ -0,0 +1,14 @@
+package Exercicios13072022;
+
+public class Exercicio2Parte2 {
+	public static void main(String[] args) {	
+		Exercicio2Parte1 motor = new Exercicio2Parte1();
+		
+		motor.Intmotor(null);
+		motor.combust(0);
+		motor.Piloto(null);
+	
+	
+	}
+
+}
